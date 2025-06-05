62154cc1a679ec08bf7bc44a5cd25b9ba076cbd2
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2025-06-05 10:55:24 +0530

    Update COLLEGE QUERY.sql

--- COLLEGE QUERY.sql
+++ COLLEGE QUERY.sql
@@ -5,7 +5,7 @@ id INT PRIMARY KEY,
 name VARCHAR(50),
 age INT NOT NULL
 );
-INSERT INTO student VALUES(1,'aman',24);
+INSERT INTO student VALUES(1,'aman',24);  
 INSERT INTO student VALUES(2,'ayush',23);
 INSERT INTO student VALUES(3,'raman',22);
 INSERT INTO student VALUES(4,'rohit',26);
